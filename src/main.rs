09c4b3714ae4bd7c103ff34101d1701533689853
mcorbin <corbin.math@gmail.com>
2017-03-30 20:14:06 +0200

    Fix bug and refacto

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,6 @@ pub mod tcp;
 use futures::Future;
 use tokio_core::reactor::Core;
 use tokio_service::Service;
-use std::thread;
-use std::time::Duration;
 use protobuf::{RepeatedField, Message};
 
 fn main() {
@@ -31,41 +29,39 @@ fn main() {
     msg.set_events(RepeatedField::from_vec(vec![e]));
 
     let size = msg.compute_size();
-    let frame = tcp::MessageFrame {
+    let frame = client::MessageFrame {
         message: msg,
         length: size
     };
 
-    // let mut e2 = proto::proto::Event::new();
-    // e2.set_state("critical".to_owned());
-    // e2.set_service("rust222".to_owned());
-    // e2.set_host("bar".to_owned());
+    let mut e2 = proto::proto::Event::new();
+    e2.set_state("critical".to_owned());
+    e2.set_service("rust222".to_owned());
+    e2.set_host("bar".to_owned());
 
-    // let mut msg2 = proto::proto::Msg::new();
-    // msg2.set_events(RepeatedField::from_vec(vec![e2]));
+    let mut msg2 = proto::proto::Msg::new();
+    msg2.set_events(RepeatedField::from_vec(vec![e2]));
 
-    // let size2 = msg2.compute_size();
-    // let frame2 = tcp::MessageFrame {
-    //     message: msg2,
-    //     length: size2
-    // };
+    let size2 = msg2.compute_size();
+    let frame2 = client::MessageFrame {
+        message: msg2,
+        length: size2
+    };
 
     let handle = core.handle();
     let addr = "127.0.0.1:5555".parse().unwrap();
-    let result = core.run(
-        tcp::Client::connect(&addr, &handle)
+    let _ = core.run(
+        tcp::TcpClient::connect(&addr, &handle)
             .and_then(|client| {
                 client.call(frame)
                     .and_then(move |response| {
                         println!("CLIENT: {:?}", response);
-                        //                        client.call(frame2)
+                        client.call(frame2)
+                    })
+                    .and_then(|response| {
+                        println!("CLIENT: {:?}", response);
                         Ok(())
                     })
-                    // .and_then(|response| {
-                    //     println!("CLIENT: {:?}", response);
-                    //     Ok(())
-                    // })
             })
     );
-    println!("RESULT: {:?}", result);
 }
--- src/tcp.rs
+++ src/tcp.rs
@@ -1,127 +1,41 @@
-use event;
-use proto::proto;
-extern crate tokio_proto;
-extern crate tokio_service;
 use std::io;
-use byteorder::ReadBytesExt;
-use byteorder::WriteBytesExt;
-use protobuf::Message;
-use byteorder::BigEndian;
-use protobuf;
 use tokio_core::net::TcpStream;
-use tokio_proto::{TcpClient, TcpServer};
-use bytes::{BytesMut};
+use tokio_proto;
 use tokio_core::reactor::Handle;
-use tokio_io::{AsyncRead, AsyncWrite};
-use tokio_io::codec::{Encoder, Decoder, Framed};
-use tokio_proto::pipeline::{ServerProto, ClientProto, ClientService};
-use tokio_service::{Service, NewService};
+use tokio_proto::pipeline::{ClientService};
+use tokio_service::{Service};
 use std::net::SocketAddr;
-use futures::{future, Future};
+use futures::{Future};
+use client;
 
-const LENGTH_LEN: usize = 4;
+// client
 
-pub struct MessageCodec;
-
-#[derive(Debug)]
-pub struct MessageFrame {
-    pub message: proto::Msg,
-    pub length: u32
+pub struct TcpClient {
+    inner: ClientService<TcpStream, client::RiemannProto>,
 }
 
-impl Encoder for MessageCodec {
-    type Item = MessageFrame;
-    type Error = io::Error;
-
-    fn encode(&mut self, msg: MessageFrame, buf: &mut BytesMut) -> io::Result<()> {
-        let mut len_writer = vec![];
-        len_writer.write_u32::<BigEndian>(msg.length)?;
-        buf.extend(len_writer);
-        let mut content_writer = vec![];
-        msg.message.write_to_vec(&mut content_writer);
-        buf.extend(content_writer);
-        Ok(())
-    }
-}
-
-impl Decoder for MessageCodec {
-    type Item = MessageFrame;
-    type Error = io::Error;
-
-    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<MessageFrame>> {
-        if buf.len() < LENGTH_LEN {
-            // wait for length
-            Ok(None)
-        }
-        else {
-            let msg = (&buf[0..4]).read_u32::<BigEndian>();
-            match msg {
-                Ok(cl) => {
-                    let content_length = cl as usize;
-                    let total_len = LENGTH_LEN + content_length;
-                    if buf.len() < total_len {
-                        // wait for rest of msg
-                        Ok(None)
-                    }
-                    else {
-                        let message = protobuf::parse_from_bytes::<proto::Msg>(&buf[LENGTH_LEN..total_len]);
-                        match message {
-                            Ok(msg) => Ok(Some(MessageFrame {
-                                message: msg,
-                                length: cl})),
-                            Err(_) => Err(io::Error::new(io::ErrorKind::Other,
-                                             "Error proto msg content"))
-                        }
-                    }
-                },
-                Err(_) => Err(io::Error::new(io::ErrorKind::Other,
-                                             "Error proto msg size"))
-            }
-        }
-    }
-}
-
-// proto
-
-pub struct RiemannProto;
-
-impl<T: AsyncRead + AsyncWrite + 'static> ClientProto<T> for RiemannProto {
-    type Request = MessageFrame;
-    type Response = MessageFrame;
-
-    /// `Framed<T, LineCodec>` is the return value of `io.framed(LineCodec)`
-    type Transport = Framed<T, MessageCodec>;
-    type BindTransport = Result<Self::Transport, io::Error>;
-
-    fn bind_transport(&self, io: T) -> Self::BindTransport {
-        Ok(io.framed(MessageCodec))
-    }
-}
-
-pub struct Client {
-    inner: ClientService<TcpStream, RiemannProto>,
-}
-
-impl Client {
+impl TcpClient {
     /// Establish a connection to a line-based server at the provided `addr`.
-    pub fn connect(addr: &SocketAddr, handle: &Handle) -> Box<Future<Item = Client, Error = io::Error>> {
-        let ret = TcpClient::new(RiemannProto)
+    pub fn connect(addr: &SocketAddr, handle: &Handle) -> Box<Future<Item = TcpClient, Error = io::Error>> {
+        let ret = tokio_proto::TcpClient::new(client::RiemannProto)
             .connect(addr, handle)
             .map(|client_service| {
-                Client { inner: client_service }
+                TcpClient { inner: client_service }
             });
         Box::new(ret)
     }
 }
 
-impl Service for Client {
-    type Request = MessageFrame;
-    type Response = MessageFrame;
+impl Service for TcpClient {
+    type Request = client::MessageFrame;
+    type Response = client::MessageFrame;
     type Error = io::Error;
     // For simplicity, box the future.
-    type Future = Box<Future<Item = MessageFrame, Error = io::Error>>;
+    type Future = Box<Future<Item = client::MessageFrame, Error = io::Error>>;
 
-    fn call(&self, req: MessageFrame) -> Self::Future {
-        Box::new(self.inner.call(req))
+    fn call(&self, req: client::MessageFrame) -> Self::Future {
+        let b = Box::new(self.inner.call(req).and_then(|resp| {
+            Ok(resp)
+        }));        b
     }
 }
