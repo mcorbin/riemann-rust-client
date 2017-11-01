e8f91e8197928e151c56ad32d79d4fd116a4b591
mcorbin <corbin.math@gmail.com>
2017-11-01 13:09:39 +0100

    It compile ;)

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ use std::net::SocketAddr;
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml).get_matches();
-    if let Some(matches) = matches.subcommand_matches("send") {
-        let protocol = matches.value_of("protocol").unwrap();
-        let riemann_server = matches.value_of("server").unwrap_or("127.0.0.1");
-        let port = matches.value_of("port").unwrap_or("5555");
-        let addr: SocketAddr = format!("{}:{}", riemann_server, port).parse().unwrap();
-        let event = cli::get_event(&matches).unwrap();
-    }
+    // if let Some(matches) = matches.subcommand_matches("send") {
+    //     let protocol = matches.value_of("protocol").unwrap();
+    //     let riemann_server = matches.value_of("server").unwrap_or("127.0.0.1");
+    //     let port = matches.value_of("port").unwrap_or("5555");
+    //     let addr: SocketAddr = format!("{}:{}", riemann_server, port).parse().unwrap();
+    //     let event = cli::get_event(&matches).unwrap();
+    // }
 }
